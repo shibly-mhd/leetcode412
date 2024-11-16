d9e7a35d1c773edb8cb20072421fbebbde5b837e
shibly-mhd <shibilik26075@gmail.com>
2024-11-16 14:38:10 +0530

    first commit

--- 1.dart
+++ 1.dart
@@ -14,27 +14,6 @@ void main() {
       print(arr);
     }
   }
-} //closing loop
+} 
 
-// /**
-//  * @param {number} n
-//  * @return {string[]}
-//  */
-// var fizzBuzz = function (n) {
-//     const arr = []
-//     for (var i = 1; i <= n; i++) {
-//         if (i % 3 === 0 && i % 5 === 0) {
-//            arr.push("FizzBuzz")
-//         }
-//         else if (i % 3 == 0) {
-//             arr.push("Fizz")
-//         }
-//         else if (i % 5 == 0) {
-//             arr.push("Buzz")
-//         }
-//         else{
-//               arr.push(i.toString())
-//         }
-//     }
-//     return arr;
-// }
+
